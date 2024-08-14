2cfaee862a6781c0bf425b21320f74c7c88ffc91
Cristhian252005 <cristhiancarrillolocopaul@gmail.com>
2024-08-14 12:51:06 -0500

    logica

--- main.cpp
+++ main.cpp
@@ -5,146 +5,95 @@
 using namespace std;
 
 struct player {
-    string nombre_jugador = "Versha";
+    string nombre_jugador;
     int score = 0;
     int id_player = 0;
     int vidas = 2;
+    int comodines[3] = {1, 2, 3};
 };
 
 struct questions {
     string preguntas;
     string opciones[4];
-    string opcion_correcta = "a";
+    char opcion_correcta = 'a';
 };
 
+const int cantidad_pre = 15;
 player *jugadores = new player[25];
-questions *preguntas_juego = new questions[15];
+questions *preguntas_juego = new questions[cantidad_pre];
+//variables
+int opcion_random;
+int id = 0;
+int tabla[25];
 
-void Registro_datos(int id) {
-    cout << "Ingreso del apodo del jugador " << id + 1 << ": ";
-    cin >> jugadores[id].nombre_jugador;
-    jugadores[id].id_player = id;
+//funciones
+void insercion_datos(int casos = 0);
+
+char string_char(const string &opcion);
+
+void menu();
+
+void Registro_datos(int id);
+
+void partida(int id);
+
+int comodines(int tipo, int pregunta);
+
+char string_char(const string &opcion) {
+    return opcion[0];
 }
-
-void juego(int id_player) {
-}
-
-void insercion_datos(int casos, int id_) {
+void puntajes(int id);
+void insercion_datos(int casos) {
     switch (casos) {
         case 1: {
             ifstream preguntas("banco.txt");
-            for (int i = 0; i < 15; i++) {
+            for (int i = 0; i < cantidad_pre; i++) {
                 getline(preguntas, preguntas_juego[i].preguntas);
                 for (int j = 0; j < 4; j++) {
                     getline(preguntas, preguntas_juego[i].opciones[j]);
                 }
             }
             preguntas.close();
+            string opcion_correcta;
+            ifstream opcionc("opciones.txt");
+            for (int i = 0; i < cantidad_pre; i++) {
+                getline(opcionc, opcion_correcta);
+                preguntas_juego[i].opcion_correcta = string_char(opcion_correcta);
+            }
+            opcionc.close();
             break;
         }
         case 2: {
-            ifstream opcionc("opciones.txt");
-            for (int i = 0; i < 15; i++) {
-                getline(opcionc, preguntas_juego[i].opcion_correcta);
-            }
-            opcionc.close();
-        }
-        case 3: {
             ofstream puntuaciones_G("tabla_Pos.txt");
-            puntuaciones_G << "Jugador: " << jugadores[id_].nombre_jugador << "  Puntuacion: " << jugadores[id_].score;
+            for (int i = 0; i < id + 1; i++) {
+                tabla[i] = jugadores[i].score;
+            }
+            for (int j = 0; j < id + 1; j++) {
+                int aux;
+                for (int i = 0; i < id; i++) {
+                    //ordenamiento burbuja
+                    if (tabla[i] < tabla[i + 1]) {
+                        aux = tabla[i + 1];
+                        tabla[i + 1] = tabla[i];
+                        tabla[i] = aux;
+                    }
+                }
+            }
+            for (int i = 0; i < id + 1; i++) {
+                for (int j = 0; j < id + 1; j++) {
+                    if (jugadores[j].score == tabla[i]) {
+                        //busqueda e insercion de datos
+                        puntuaciones_G << "\nJugador: " << jugadores[j].nombre_jugador << "  Puntuacion: " << jugadores[
+                            j].score;
+                    }
+                }
+            }
         }
     }
 }
 
-void partida(int id_p) {
-    int i;
-    string opcion;
-    while (jugadores[id_p].vidas > 0 || i == 15) {
-        cout << "Puntuacion: " << jugadores[id_p].score << "               " << "Vidas: " << jugadores[id_p].vidas <<
-                endl;
-        cout << preguntas_juego[i].preguntas << endl;
-        for (int j = 0; j < 4; j++) {
-            cout << preguntas_juego[i].opciones[j] << endl;
-        }
-        cout << "\nEscoga la opcion correcta";
-        cin >> opcion;
-        if (opcion == preguntas_juego[i].opcion_correcta) {
-            jugadores[id_p].score = jugadores[id_p].score + 1000;
-        } else {
-            jugadores[id_p].vidas = jugadores[id_p].vidas - 1;
-        }
-        cout << "La opcion correcta...";
-        cout << preguntas_juego[i].opcion_correcta << endl;
-        i = i + 1;
-    }
-}
-int comodines(int tipo,int pregunta,int id_) {
-    int opcion_random;
-    string opcion;
-    string opcion_co=preguntas_juego[pregunta].opcion_correcta;
-    switch (tipo) {
-        case 1: {//Llamada al publico
-            cout<<"LLamando a un experto ..."<<endl;
-            cout<<"Yo creo que es .."<<preguntas_juego[pregunta].opcion_correcta;
-        }
-        case 2: {
-            //Graficos en ascii con casos
-            switch (opcion_co) {
-                case "a": {
-
-                }
-                case "b": {
-
-                }
-                case "c": {
-
-                }
-                case "d": {
-
-                }
-            }
-        }
-        case 3: {//50 50
-            cout << preguntas_juego[pregunta].preguntas << endl;
-            switch (preguntas_juego[pregunta].opcion_correcta) {
-                case 'a': {
-                    cout << preguntas_juego[pregunta].opciones[0] << endl;
-                    do{opcion_random=rand()%4;}while (opcion_random==0);
-                    cout << preguntas_juego[pregunta].opciones[opcion_random] << endl;
-                    break;
-                }
-                case 'b': {
-                    do{opcion_random=rand()%4;}while (opcion_random==1);
-                    cout << preguntas_juego[pregunta].opciones[1] << endl;
-                    break;
-                }
-                case 'c': {
-                    do{opcion_random=rand()%4;}while (opcion_random==2);
-                    cout << preguntas_juego[pregunta].opciones[2] << endl;
-                    break;
-                }
-                case 'd': {
-                    do{opcion_random=rand()%4;}while (opcion_random==3);
-                    cout << preguntas_juego[pregunta].opciones[3] << endl;
-                    break;
-                }
-            }
-            cout << "\nEscoga la opcion correcta";
-            cin >> opcion;
-            if (opcion == preguntas_juego[pregunta].opcion_correcta) {
-                jugadores[id_].score = jugadores[id_].score + 1000;
-            } else {
-                jugadores[id_].vidas = jugadores[id_].vidas - 1;
-            }
-            cout << "La opcion correcta...";
-            cout << preguntas_juego[pregunta].opcion_correcta << endl;
-            return pregunta+1;
-        }
-    }
-}
 void menu() {
     int option;
-    int id = 0;
     do {
         cout << "1.Nueva Partida";
         cout << "\n2.Creditos";
@@ -152,32 +101,192 @@ void menu() {
         cout << "\n4.Salir";
         cout << "\nEscoga una opcion:";
         cin >> option;
-        switch (option) {
-            case 1: {
-                Registro_datos(id);
-                partida(id);
-                id = +0;
-                break;
-            }
-            case 2: {
-                break;
-            }
-            case 3: {
-                break;
-            }
-            case 4: {
-                break;
-            }
-            default: {
+        if (cin.fail()) {
+            //validad de entrada
+            cin.clear();
+            cin.ignore();
+            option = 5;
+            cout << "\nIngrese un numero" << endl;
+        } else {
+            switch (option) {
+                case 1: {
+                    Registro_datos(id);
+                    partida(id);
+                    id = id + 1;
+                    break;
+                }
+                case 2: {
+                    break;
+                }
+                case 3: {
+                    puntajes(id);
+                    break;
+                }
+                case 4: {
+                    break;
+                }
+                default: {
+                    cout << "Ingrese un valor valido";
+                }
             }
         }
     } while (option != 4);
 }
 
-int main() {
-    insercion_datos(1, 0);
-    menu();
+void Registro_datos(int id) {
+    cout << "Ingreso del apodo del jugador " << id + 1 << ": ";
+    cin >> jugadores[id].nombre_jugador;
+    jugadores[id].id_player = id;
+}
 
-    cout << "hoasdasdasdla" << endl;
+void partida(int id) {
+    int i = 0;
+    string opcion_string;
+    char opcion_char;
+    while (jugadores[id].vidas > 0 || i == cantidad_pre) {
+        cout << "Puntuacion: " << jugadores[id].score << "               " << "Vidas: " << jugadores[id].vidas <<
+                endl;
+        cout << preguntas_juego[i].preguntas << endl;
+        for (int j = 0; j < 4; j++) {
+            cout << preguntas_juego[i].opciones[j] << endl;
+        }
+        //comodines
+        if (jugadores[id].comodines[0] != 0) {
+            cout << "1. Llamada a un experto";
+        }
+        if (jugadores[id].comodines[1] != 0) {
+            cout << "\t2. Llamada al publico";
+        }
+        if (jugadores[id].comodines[2] != 0) {
+            cout << "\t3. 50/50";
+        }
+        cout << "\nEscoga la opcion correcta o un comodin:";
+        cin >> opcion_string;
+        opcion_char=string_char(opcion_string);
+        switch (opcion_char) {
+            case '1': {
+                i = comodines(1, i);
+                break;
+            }
+            case '2': {
+                i = comodines(2, i);
+                break;
+            }
+            case '3': {
+                i = comodines(3, i);
+                break;
+            }
+            default: {
+                if (opcion_char == preguntas_juego[i].opcion_correcta) {
+                    jugadores[id].score = jugadores[id].score + 1000;
+                    cout << "Felicitaciones Acertaste";
+                } else {
+                    jugadores[id].vidas = jugadores[id].vidas - 1;
+                    cout << "\nFallaste";
+                    cout << "\nLa opcion correcta...";
+                    cout << preguntas_juego[i].opcion_correcta << endl;
+                }
+                i = i + 1;
+            }
+        }
+    }
+    insercion_datos(2);
+}
+
+int comodines(int tipo, int pregunta) {
+    string opcion_string;
+    char opcion_char;
+    char opcion_co = preguntas_juego[pregunta].opcion_correcta;
+    switch (tipo) {
+        case 1: {
+            //Llamada al publico
+            if (jugadores[id].comodines[0] != 0) {
+                cout << "\nLLamando a un experto ..." << endl;
+                cout << "Yo creo que es .." << preguntas_juego[pregunta].opcion_correcta << endl;
+                jugadores[id].comodines[0] = 0;
+            }
+            return pregunta;
+        }
+        case 2: {
+            //Graficos en ascii con casos
+            if (jugadores[id].comodines[1] != 0) {
+                switch (opcion_co) {
+                    case 'a': {
+                        break;
+                    }
+                    case 'b': {
+                        break;
+                    }
+                    case 'c': {
+                        break;
+                    }
+                    case 'd': {
+                        break;
+                    }
+                }
+            }
+            jugadores[id].comodines[1] = 0;
+            return pregunta;
+        }
+        case 3: {
+            //50 50
+            if (jugadores[id].comodines[2] != 0) {
+                cout << preguntas_juego[pregunta].preguntas << endl;
+                switch (preguntas_juego[pregunta].opcion_correcta) {
+                    case 'a': {
+                        cout << preguntas_juego[pregunta].opciones[0] << endl;
+                        do { opcion_random = rand() % 4; } while (opcion_random == 0);
+                        cout << preguntas_juego[pregunta].opciones[opcion_random] << endl;
+                        break;
+                    }
+                    case 'b': {
+                        do { opcion_random = rand() % 4; } while (opcion_random == 1);
+                        cout << preguntas_juego[pregunta].opciones[1] << endl;
+                        cout << preguntas_juego[pregunta].opciones[opcion_random] << endl;
+                        break;
+                    }
+                    case 'c': {
+                        do { opcion_random = rand() % 4; } while (opcion_random == 2);
+                        cout << preguntas_juego[pregunta].opciones[2] << endl;
+                        cout << preguntas_juego[pregunta].opciones[opcion_random] << endl;
+                        break;
+                    }
+                    case 'd': {
+                        do { opcion_random = rand() % 4; } while (opcion_random == 3);
+                        cout << preguntas_juego[pregunta].opciones[opcion_random] << endl;
+                        cout << preguntas_juego[pregunta].opciones[3] << endl;
+                        break;
+                    }
+                }
+                cout << "\nEscoga la opcion correcta:";
+                cin >> opcion_string;
+                opcion_char=string_char(opcion_string);
+                if (opcion_char == preguntas_juego[pregunta].opcion_correcta) {
+                    jugadores[id].score = jugadores[id].score + 1000;
+                    cout << "Felicitaciones Acertaste";
+                } else {
+                    jugadores[id].vidas = jugadores[id].vidas - 1;
+                    cout << "\nFallaste";
+                    cout << "\nLa opcion correcta...";
+                    cout << preguntas_juego[pregunta].opcion_correcta << endl;
+                }
+                jugadores[id].comodines[2] = 0;
+                return pregunta + 1;
+            }
+        }
+    }
+}
+void puntajes(int id) {
+    string puntajes[25];
+    ifstream posiciones("tabla_Pos.txt");
+    for (int i=0;i<5;i++) {
+        getline(posiciones,puntajes[i]);
+        cout<<puntajes[i]<<endl;
+    }
+}
+
+int main() {
+    insercion_datos(1);
+    menu();
     return 0;
 }
