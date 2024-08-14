001433db9200c8206bfa9d5a999deba6a382d385
Cristhian Carrillo <cristhiancarrillolocopaul@gmail.com>
2024-08-14 16:07:54 -0500

    demo

---
+++ cmake-build-debug/CMakeFiles/Versha.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Versha.
+# This may be replaced when dependencies are built.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/crist/CLionProjects/Versha
+# Install script for directory: C:/Users/crist/CLionProjects/Punteros/Versha5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/crist/CLionProjects/Versha/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/crist/CLionProjects/Punteros/Versha5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
